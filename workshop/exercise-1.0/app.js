f6a49d16ebf6a2882758b8e74ceec4e9e30464f8
Joel <joel.m.bernstein@gmail.com>
2020-10-04 13:33:37 -0400

    completed exercise-1.0

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,14 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+
+
+
+let body = document.querySelector('body');
+
+function handleClick() {
+   let main = document.querySelector('#main');
+   main.innerText = ("You win!");
+}
+
+body.addEventListener('click', handleClick);
